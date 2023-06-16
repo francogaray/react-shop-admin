e4a209e5c7decbc832f53e81a51c39dc2640cb67
francogaray <francogaray.dev@gmail.com>
2023-06-16 13:07:00 -0300

    config tailwind

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+module.export = {
+  semi: true,
+  sigleQuote: true,
+  printWidth: 200,
+  tabWidth: 4,
+  trailingComma: "es5",
+  bracketSpacing: true,
+};
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+
+const colors = require("tailwindcss/colors");
+
+delete colors["lightBlue"];
+delete colors["warmGray"];
+delete colors["trueGray"];
+delete colors["coolGray"];
+delete colors["blueGray"];
+
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        ...colors,
+        green: colors.emerald,
+        yellow: colors.amber,
+        purple: colors.violet,
+      },
+    },
+  },
+  plugins: [],
+};
